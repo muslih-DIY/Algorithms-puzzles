74e32db5a7b224ae58d8456b4932abaae2d13f07
muslih_k_diy <muslihk00@gmail.com>
2023-07-21 22:35:42 +0530

    aoc-2016-day1-part2 addded

--- DS&ALGO/Aoc/2016/day1/solution.py
+++ DS&ALGO/Aoc/2016/day1/solution.py
@@ -1,42 +1,107 @@
 from input import direction
 
-direction_array = direction.replace(' ','').split(',')
-
-
-START_POINT = (0,0)
-
-# current facing direction
-fd = 'N'
-
-# current position
-cp = START_POINT
-
-
-# direction data
 
 
 
-DIRECTION_INFO = {
-    'N':{'L':[(-1,0),'W'],'R':[(1,0),'E']},
-    'W':{'L':[(0,-1),'S'],'R':[(0,1),'N']},
-    'S':{'L':[(1,0),'E'],'R':[(-1,0),'W']},
-    'E':{'L':[(0,1),'N'],'R':[(0,-1),'S']}
-    }
+def part1():
+
+    direction_array = direction.replace(' ','').split(',')
 
 
-## loop  move the direction
+    START_POINT = (0,0)
 
-for MV in direction_array:
-    #move direction ,move step
-    MD,MS = MV[:1],int(MV[1:])
+    # current facing direction
+    fd = 'N'
 
-    # moving points, new facing direction
-    mp,fd_new = DIRECTION_INFO[fd][MD]
+    # current position
+    cp = START_POINT
 
-    cp = (cp[0]+MS*mp[0],cp[1]+MS*mp[1])
-    fd = fd_new
 
-print(fd,cp)
+    # direction data
 
-print(abs(cp[0]-0)+abs(cp[1]-0))
+
+
+    DIRECTION_INFO = {
+        'N':{'L':[(-1,0),'W'],'R':[(1,0),'E']},
+        'W':{'L':[(0,-1),'S'],'R':[(0,1),'N']},
+        'S':{'L':[(1,0),'E'],'R':[(-1,0),'W']},
+        'E':{'L':[(0,1),'N'],'R':[(0,-1),'S']}
+        }
+
+
+    ## loop  move the direction
+
+    for MV in direction_array:
+        #move direction ,move step
+        MD,MS = MV[:1],int(MV[1:])
+
+        # moving points, new facing direction
+        mp,fd_new = DIRECTION_INFO[fd][MD]
+
+        cp = (cp[0]+MS*mp[0],cp[1]+MS*mp[1])
+        fd = fd_new
+
+    print(fd,cp)
+
+    print(abs(cp[0]-0)+abs(cp[1]-0))
     
+
+def part2():
+
+    direction_array = direction.replace(' ','').split(',')
+
+
+    START_POINT = (0,0)
+
+    # current facing direction
+    fd = 'N'
+
+    # current position
+    cp = START_POINT
+
+
+    # direction data
+
+
+
+    DIRECTION_INFO = {
+        'N':{'L':[(-1,0),'W'],'R':[(1,0),'E']},
+        'W':{'L':[(0,-1),'S'],'R':[(0,1),'N']},
+        'S':{'L':[(1,0),'E'],'R':[(-1,0),'W']},
+        'E':{'L':[(0,1),'N'],'R':[(0,-1),'S']}
+        }
+
+
+    visisted_locations = [cp]
+
+
+    ## loop  move the direction
+
+    for MV in direction_array:
+        #move direction ,move step
+        MD,MS = MV[:1],int(MV[1:])
+
+        # moving points, new facing direction
+        mp,fd_new = DIRECTION_INFO[fd][MD]
+        found =False
+        # cp = (cp[0]+MS*mp[0],cp[1]+MS*mp[1])
+        for _ in range(MS):
+            cp = (cp[0]+mp[0],cp[1]+mp[1])
+            if cp in visisted_locations:
+                found=True
+                break
+            visisted_locations.append(cp)
+        
+        fd = fd_new
+        if found:
+            break
+
+    print(visisted_locations)    
+
+    print(fd,cp)
+    print(abs(cp[0]-0)+abs(cp[1]-0))
+    
+
+if __name__=='__main__':
+
+    part2()
